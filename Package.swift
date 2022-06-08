a033ce1647741a63a00de79ba59552be2dc40294
Renetik <dohan.rene@gmail.com>
2022-06-08 10:27:57 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["RenetikLayout"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/renetik/renetik-ios-core", branch: "master"),
+        .package(url: "https://github.com/renetik/renetik-ios-core", .exact("0.9.0")),
     ],
     targets: [
         .target(
