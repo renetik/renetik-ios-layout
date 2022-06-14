6f826a4bb38ef74b49496b2efb35d3181139a8ba
Rene Dohan <dohan.rene@gmail.com>
2022-06-14 14:17:58 -0500

    Update UIView+CSSafeArea.swift

--- Sources/RenetikLayout/extensions/UIKit/UIView+CSSafeArea.swift
+++ Sources/RenetikLayout/extensions/UIKit/UIView+CSSafeArea.swift
@@ -1,7 +1,7 @@
 extension UIView {
     public var topInset: CGFloat { safeArea.top }
     public var bottomInset: CGFloat { safeArea.bottom }
-    public var rightInset: CGFloat { window!.isLandscapeLeft ? safeArea.right : 0 }
-    public var leftInset: CGFloat { window!.isLandscapeRight ? safeArea.left : 0 }
+    public var rightInset: CGFloat { window?.isLandscapeLeft == true ? safeArea.right : 0 }
+    public var leftInset: CGFloat { window?.isLandscapeRight == true ? safeArea.left : 0 }
     public var safeArea: UIEdgeInsets { window?.safeAreaInsets ?? .zero }
 }
